d60036a585b38542fa9a0aad40d14c150d5256a0
EsterDahri <esterdahri@gmail.com>
2023-10-27 17:39:46 +0200

    add app.blade

---
+++ app/Http/Controllers/Guests/PageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Guests;
+
+use App\Http\Controllers\Controller;
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        return view('welcome', ['movies' => Movie::orderByDesc('id')->limit(4)->get()]);
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('page-title', 'Model Controllers')</title>
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+</head>
+
+<body class="">
+    <!--  includiamo l'header del sito web con il menu tramite un partial -->
+    @include('partials.header')
+
+    <main>
+        @yield('content')
+    </main>
+
+    <!-- includiamo il footer del sito web tramite un partial -->
+    @include('partials.footer')
+
+</body>
+
+</html>
